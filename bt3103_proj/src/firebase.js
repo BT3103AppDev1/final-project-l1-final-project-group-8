f2cea3230f9bd2bbea3d337f3216cad9c2631a4a
Sofia <stormyferntreeofficial@gmail.com>
2023-10-24 15:13:48 +0800

    Update firebase.js

---
+++ bt3103_proj/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGc6J6-I2Yg7oQ6mreqDcBUzT-sH0bZTM",
+  authDomain: "bt3103-project-cb36b.firebaseapp.com",
+  projectId: "bt3103-project-cb36b",
+  storageBucket: "bt3103-project-cb36b.appspot.com",
+  messagingSenderId: "932977578520",
+  appId: "1:932977578520:web:82a495bb026f918c16e96d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
