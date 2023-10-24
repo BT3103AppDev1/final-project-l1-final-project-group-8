7c9a8c02bf4ae184571815a19be22e73a9a559a4
Sofia <stormyferntreeofficial@gmail.com>
2023-10-24 15:19:34 +0800

    Update uifirebase.js
    added code to uifirebase hope it works tho

---
+++ bt3103_proj/src/uifirebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/compat/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBGc6J6-I2Yg7oQ6mreqDcBUzT-sH0bZTM",
+    authDomain: "bt3103-project-cb36b.firebaseapp.com",
+    projectId: "bt3103-project-cb36b",
+    storageBucket: "bt3103-project-cb36b.appspot.com",
+    messagingSenderId: "932977578520",
+    appId: "1:932977578520:web:82a495bb026f918c16e96d"
+}
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
