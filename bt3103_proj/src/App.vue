63e84e4711e831f1e5581afbb3aac0b63ee56c54
jefflimwz <100202288+jefflimwz@users.noreply.github.com>
2023-10-24 17:25:12 +0800

    Added sidebar component

--- bt3103_proj/src/App.vue
+++ bt3103_proj/src/App.vue
@@ -1,47 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import login_sidebar from './components/login_sidebar.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <login_sidebar/>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+<style>
+#patientBlock {
+  background-color: white;
+  p {
+    font-weight: bold;
   }
 }
 </style>
