0f38600762d81eefe5871ecea5c9f041abbbe4ee
Sofia <stormyferntreeofficial@gmail.com>
2023-11-01 23:19:40 +0800

    Sofia portions
    Added Appointment form, Patient details edit and search patient (table unfinished). front end stuff for now

--- bt3103_proj/src/components/Patient_edit.vue
+++ bt3103_proj/src/components/Patient_edit.vue
@@ -7,10 +7,75 @@
                     <div class = "title">NAME:</div>
                     <div id = "nameText">test</div> <!--Hard code for now-->
                 </div>
+
+                <div id = "dob">
+                    <div class = "title">DATE OF BIRTH:</div>
+                    <div id = "dobText">test</div> <!--Hard code for now-->
+                </div>     
+                
+                <div id = "contactNum" v-if="edit = edit">
+                    <div class = "title">CONTACT NUM:</div>
+                    <form action="">
+                        <input type="text" name="phoneNum" id="phoneNum">
+                    </form>
+                </div>
+
+                <div v-else id = "contactNum" >
+                    <div class = "title">CONTACT NUM:</div>
+                    <div id = "numText">1234</div> <!--Hard code for now-->
+                </div>
+
+                <div id = "ic">
+                    <div class = "title">PATIENT IC:</div>
+                    <div id = "icText">1234</div> <!--Hard code for now-->
+                </div>
+
+                <div id = "gender">
+                    <div class = "title">GENDER:</div>
+                    <div id = "genderText">1234</div> <!--Hard code for now-->
+                </div>
+
+                <div id = "blood">
+                    <div class = "title">BLOOD TYPE:</div>
+                    <div id = "bloodText">1234</div> <!--Hard code for now-->
+                </div>
+            </div>
+
+            <div  id = "button" v-if="edit = edit">
+                <div id = "actionEditBtn">
+                    <button id = "submitButton" type="button">Submit</button>
+                    <button id = "cancelButton" type = "button" @click="cancelEdit">Cancel</button>
+                </div>
+            </div>
+
+            <div v-else id="button" >
+                <div id = "actionBtn">
+                    <button id = "editButton" type="button" v-on:click="allowEdit">Edit Details</button>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            edit : false,
+        }
+    },
+
+    methods:{
+        allowEdit() {
+            this.edit = true
+        },
+
+        cancelEdit() {
+            this.edit = false
+        }
+    }
+}
+</script>
 
 <style scoped>
 #rectangle {
@@ -21,12 +86,62 @@
     border-radius: 67px;
 }
 
-#mega {
+button {
+    width:137px;
+    height: 29px;
+    background: #8FBC94; 
+    border: none;
+    border-radius: 6px;
+    font-weight:600;
+}
+
+button:hover {
+    background: #d7e7d9;
+    font-weight: 600; 
+}
+
+#button {
     position: relative;
+    top: 16em;
+}
+
+#editButton {
+    position: relative;
+    left: 30em;
+}
+
+#actionEditBtn {
+    position: relative;
+    left: 20em;
+}
+
+#submitButton {
+    margin-right: 0.5em;
+}
+
+#cancelButton {
+    margin-left: 0.5em;
+    background: #FF0000;
+}
+
+#cancelButton:hover {
+    background-color: #ffaeae;
+}
+
+#mega {
+    position: absolute;
     left: 20em;
     top:2em;
 }
 
+.title {
+    white-space: nowrap;
+    color: #464E5F;
+    font-size: 14px;
+    font-family: Poppins, Arial, Helvetica, sans-serif;
+    font-weight: 600;
+    word-wrap: break-word;
+}
 #patDet {
     position: absolute;
     left: 5em;
@@ -34,6 +149,36 @@
 }
 
 #nameText {
+    position: absolute;
+    left: 10rem;
+    top: 0.01em;
+    overflow: visible;
+
+}
+
+#contactNum {
+    position: absolute;
+    top: 7em;
+}
+
+#numText {
+    position: absolute;
+    left: 10rem;
+    top: 0.01em;
+    overflow: visible;
+}
+
+#phoneNum {
+    position: absolute;
+    left: 10rem;
+    top: 0.01em;
+}
+#dob {
+    position: absolute;
+    top: 3.5em;
+
+}
+#dobText {
     position: absolute;
     left: 10rem;
     top: 0.01em;
@@ -52,4 +197,31 @@
     line-height: 20px;
     word-wrap: break-word
 }
+
+#ic, #gender, #blood {
+    position: absolute;
+    left: 27em;
+    top:0.01em;
+}
+
+#ic {
+    top: 0.01em;
+}
+
+#gender {
+    position: absolute;
+    top: 3.5em;
+}
+
+#blood {
+    position: absolute;
+    top: 7em;
+}
+
+#icText, #genderText, #bloodText {
+    position: inherit;
+    left : 10em;
+    bottom: 0.01em;
+}
+
 </style>
---
+++ bt3103_proj/src/components/SearchPatient.vue
@@ -0,0 +1,123 @@
+<template>
+<div id = "mega">
+    <div id = "rectangle">
+        <div id = "content">
+            <div id = "Title">
+                Search Patient
+            </div>
+            <div id = "searchBar">
+                <form action="">
+                        <input type="text" name="patID" id="patID" placeholder="Enter Patient IC">
+                </form>
+            </div>
+            <div id = "searchButt">
+                <button id = "searchButton" type="button">Search</button>
+            </div>
+        </div>
+    </div>
+
+    <div id = "resultRect">
+        <div id = "resultTable">
+            <table>
+                <tr id="tableHeadRow">
+                    <th>Name</th>
+                    <th>ID</th>
+                    <th>Phone Number</th>
+                    <th>Add Patient</th>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<style scoped>
+#mega {
+    position: absolute;
+    left: 20em;
+    top:5em;
+}
+
+#rectangle {
+    height: 100px;
+    width: 400px;
+    background: #F7F7F7; 
+    border-radius: 20px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.50); 
+
+}
+
+#content {
+    left:1em;
+    top:1em;
+    position: relative;
+}
+
+#Title {
+    color: #212121;
+    font-size: 18px;
+    font-family: Poppins, Arial, Helvetica, sans-serif;
+    font-weight: 500;
+    line-height: 21px;
+    word-wrap: break-word;
+    font-weight:600;
+}
+
+#searchBar {
+    position: relative;
+    top: 0.5em;
+}
+
+#patID {
+    background: white; 
+    border-radius: 10px; 
+    border: 1px #C5C5C5 solid;
+    height: 25px;
+    width: 200px;
+}
+
+#searchButt {
+    position: relative;
+    left: 16rem;
+    bottom: 1.44rem;
+}
+
+#searchButton {
+    background: #8FBC94; 
+    border-radius: 6px;
+    height: 29px;
+    width: 86px;
+    border: none;
+    font-weight: 600;
+}
+
+#searchButton:hover {
+    background: #d7e7d9; 
+    border-radius: 6px;
+    height: 29px;
+    width: 86px;
+    border: none;
+    font-weight: 600;
+    
+}
+
+#resultRect {
+    position: inherit;
+    top: 8rem;
+    background: white; 
+    box-shadow: 0px 0px 30px rgba(56.09, 71.02, 109.44, 0.09); 
+    border-radius: 12px; 
+    overflow: hidden;
+}
+
+#tableHeadRow {
+    background: #B5B5C3; 
+    border-collapse: collapse;
+    border-radius: 6px;
+    font: 1em sans-serif;
+}
+
+
+
+</style>
