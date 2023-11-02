298cc8f26b07829b924876f0c62f3f6b9909d9cd
beneciatang <46887929+beneciatang@users.noreply.github.com>
2023-11-02 13:29:08 +0800

    edit firebase and ui firebase with log in functions

--- bt3103_proj/src/firebase.js
+++ bt3103_proj/src/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from   "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,7 +13,32 @@ const firebaseConfig = {
   storageBucket: "bt3103-project-cb36b.appspot.com",
   messagingSenderId: "932977578520",
   appId: "1:932977578520:web:82a495bb026f918c16e96d"
-};
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+//create Users
+const auth = getAuth(app);
+createUserWithEmailAndPassword(auth, email, password)
+.then((userCredential) => {
+  //Signed in
+  const user = userCredential.user;
+})
+
+// Sign-in Users
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+signInWithEmailAndPassword(auth, email, password)
+.then((userCredential) => {
+  //Signed in 
+  const user = userCredential.user;
+})
+
+import { getAuth, signOut } from "firebase/auth";
+
+signOut(auth).then(() => {
+  //Sign out successful.
+})
+
+export { auth }
